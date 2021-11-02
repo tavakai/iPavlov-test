553fda727cdec294be9cd7265572243d39594766
Amyrga Tavakai <tavakaiam@yandex.ru>
2021-11-02 21:30:57 +0300

    remove methods

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -61,17 +61,7 @@ export const getUserProfile = () => {
       return _getResponseData(res)
     })
 }
-// Получить сохраненные фильмы
-export const getSavedItems = () => {
-  return fetch(`${base_url}/movies`, {
-    method: 'GET',
-      headers: headers,
-      credentials: 'include',
-    })
-    .then(res => {
-      return _getResponseData(res)
-    });
-}
+
 // Изменить профиль пользователя
 export const doChangeUserInfo = (data) => {
   return fetch(`${base_url}/users/me`, {
@@ -82,41 +72,6 @@ export const doChangeUserInfo = (data) => {
         name: data.name,
         email: data.email
       })
-    })
-    .then(res => {
-      return _getResponseData(res)
-    })
-}
-// Создание нового фильма
-export const addItem = (data) => {
-  return fetch(`${base_url}/movies`, {
-      method: 'POST',
-      headers: headers,
-      credentials: 'include',
-      body: JSON.stringify({
-        country: data.country,
-        director: data.director,
-        duration: data.duration,
-        year: data.year,
-        description: data.description,
-        image: data.image.url,
-        trailer: data.trailerLink,
-        nameRU: data.nameRU,
-        nameEN : data.nameEN,
-        thumbnail : data.image.formats.thumbnail.url,
-        id : data.id,
-      })
-    })
-    .then(res => {
-      return _getResponseData(res)
-    })
-}
-// Удаление фильма
-export const removeItem = (id) => {
-  return fetch(`${base_url}/movies/${id}`, {
-      method: 'DELETE',
-      headers: headers,
-      credentials: 'include',
     })
     .then(res => {
       return _getResponseData(res)
